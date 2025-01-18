8b6cf2e8a61a7aac031a89c8638098a79dd11886
LillyBlueHair <155645207+LillyBlueHair@users.noreply.github.com>
2025-01-18 20:58:46 +0100

    Update loader.user.js

--- Markings/loader.user.js
+++ Markings/loader.user.js
@@ -4,22 +4,16 @@
 // @version 1.6
 // @description Bondage Club Markings for Luke's subs
 // @author LillyBlueHair
+// @downloadURL https://lillybluehair.github.io/Markings/loader.user.js
+// @updateURL https://lillybluehair.github.io/Markings/loader.user.js
 // @match https://*.bondageprojects.elementfx.com/R*/*
 // @match https://*.bondage-europe.com/R*/*
 // @match https://*.bondageprojects.com/R*/*
 // @grant none
 // @run-at document-end
 // ==/UserScript==
-
-(function () {
-    "use strict";
-    const src = `https://lillybluehair.github.io/Markings/marking.js?v=${Date.now()}`;
-    if (typeof markins_loaded === "undefined") {
-        const script = document.createElement("script");
-        script.src = src;
-        script.type = "text/javascript";
-        script.crossOrigin = "anonymous";
-        script.id = "Markings";
-        document.head.appendChild(script);
-    }
-})();
+import(
+	`https://lillybluehair.github.io/Markings/marking.js?v=${(
+		Date.now()
+	).toFixed(0)}`
+)
